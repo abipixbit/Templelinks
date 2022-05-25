06b8d74f705dca65ebc2bd4a24dc77f821838463
abi <abijith@pixbitsolutions.com>
2022-05-25 17:49:57 +0530

    Added Splash Screen

--- app/src/main/java/com/example/templelinks/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/templelinks/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.templelinks.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.templelinks.R
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
