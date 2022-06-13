9642f7d3a4144d474ebe3830d2fed62c44879428
abi <abijith@pixbitsolutions.com>
2022-06-13 17:56:32 +0530

    changed folder structure...

--- app/src/main/java/com/example/templelinks/ui/fragment/homeFragment/HomeViewModel.kt
+++ app/src/main/java/com/example/templelinks/ui/fragment/homeFragment/HomeViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.templelinks.FavouriteEventBus
 import com.example.templelinks.data.model.Banners
 import com.example.templelinks.data.model.Deities
 import com.example.templelinks.data.model.response.ApiResponse
@@ -14,10 +13,8 @@ import com.example.templelinks.data.repository.DeitiesRepository
 import com.example.templelinks.data.repository.FavouriteRepository
 import com.example.templelinks.data.repository.HomeCategoryRepository
 import com.example.templelinks.enums.ApiStatus
-import com.example.templelinks.enums.FavEvent
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 class HomeViewModel : ViewModel(), KoinComponent {
 
